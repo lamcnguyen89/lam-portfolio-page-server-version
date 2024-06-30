6f0fa65a3779b3df2f7b04666e475bdcd09f2b91
Lam C Nguyen <lamcnguyen89@gmail.com>
2024-06-30 01:21:59 -0400

    Added code to produce a JSON Webtoken from user sent password

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // Import Model
@@ -59,8 +61,23 @@ router.post(
             
 
             // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User registered!!!!');
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                {expiresIn: 360000},
+                (err, token) =>{
+                    if(err) throw err;
+                    res.json({ token})
+                }
+        );
+
+
 
         } catch(err) {
             console.error(err.message);
