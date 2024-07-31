1c01d602d9cf305dd53d2b4fc75d0c264b04ab90
Lam C Nguyen <lamcnguyen89@gmail.com>
2024-07-30 21:59:19 -0400

    Added infinite scroll

--- client/src/components/projects/ProjectPage.js
+++ client/src/components/projects/ProjectPage.js
@@ -12,22 +12,47 @@ const ProjectPage = ({ getProject, category, post: { posts, loading } }) => {
     getProject(category);
   }, [getProject]);
 
+  const itemsPerPage = 3;
+  const [hasMore, setHasMore] = useState(true);
+  const [records, setrecords] = useState(itemsPerPage);
+  const loadMore = () => {
+    if (records === posts.length) {
+      setHasMore(false);
+    } else {
+      setTimeout(() => {
+        setrecords(records + itemsPerPage);
+      }, 2000);
+    }
+  };
+
   return (
     <section className="container">
       <h1>{category}</h1>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Fragment>
-          <h1 className="large text-primary">Posts</h1>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadMore}
+        hasMore={hasMore}
+        loader={
+          <h4 className="loader" key={0}>
+            Loading...
+          </h4>
+        }
+        useWindow={false}
+      >
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Fragment>
+            <h1 className="large text-primary">Posts</h1>
 
-          {posts.length > 0 ? (
-            posts.map((post) => <ProjectItem key={post._id} post={post} />)
-          ) : (
-            <h4>No projects found...</h4>
-          )}
-        </Fragment>
-      )}
+            {posts.length > 0 ? (
+              posts.map((post) => <ProjectItem key={post._id} post={post} />)
+            ) : (
+              <h4>No projects found...</h4>
+            )}
+          </Fragment>
+        )}
+      </InfiniteScroll>
     </section>
   );
 };
